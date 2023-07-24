17ebbf153b47fef40aa11eb79213151154c37730
Gerard Fernandez Tejada <84667512+Gefete@users.noreply.github.com>
2023-07-24 22:12:50 +0200

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    firebase:{
+        apiKey: "AIzaSyAH9lnA_PoxMdcOt-IHgXZd0M4WVqO78Uo",
+        authDomain: "fe-gc-ang-auth-firebase.firebaseapp.com",
+        projectId: "fe-gc-ang-auth-firebase",
+        storageBucket: "fe-gc-ang-auth-firebase.appspot.com",
+        messagingSenderId: "207431646708",
+        appId: "1:207431646708:web:fcd891b535122f49c28da1",
+        measurementId: "G-85JVHVVS5E"
+    },
+    production: true
+};
