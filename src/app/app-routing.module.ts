9f322fd6d72670203b443ba4ababef13a44b8e37
Gerard Fernandez Tejada <84667512+Gefete@users.noreply.github.com>
2023-07-26 12:34:35 +0200

    guards ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { AboutComponent } from './home/about/about.component';
 import { CharactersComponent } from './home/characters/characters.component';
 import { DetalleComponent } from './home/characters/detalle/detalle.component';
 import { BienvenidaComponent } from './home/bienvenida/bienvenida.component';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard'
 
 
 const routes: Routes = [
@@ -15,8 +16,8 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent },
   {path: 'home', component: BienvenidaComponent},
   {path: 'about', component: AboutComponent}, 
-  {path: 'characters', component: CharactersComponent},
-  {path: 'detalles/:id', component:DetalleComponent}
+  {path: 'characters', component: CharactersComponent, ...canActivate(() => redirectUnauthorizedTo(['/login']))},
+  {path: 'detalles/:id', component:DetalleComponent, ...canActivate(() => redirectUnauthorizedTo(['/login']))}
 ];
 
 @NgModule({
